f428a2cae2db91fd3cd2d1db20c210a1fa709cff
RRON-SIU <nishorirron133@gmail.com>
2025-09-15 17:46:26 +0200

    Add files via upload

---
+++ lesson 29/js/main.js
@@ -0,0 +1,17 @@
+  var programing = ['java','php','python']
+  console.log(programing[0])
+
+  programminglanguages.push['java']
+  console.log(programming[0])
+
+  programminglanguages.pop['php']
+  console.log(programming[0])
+   
+programminglanguages.shift['python']
+  console.log(programming[0])
+
+  programminglanguages.unshift['java']
+  console.log(programming[0])
+
+  programminglanguages.splice['java']
+  console.log(programming[0])
